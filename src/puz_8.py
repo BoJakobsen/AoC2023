a570035017af4faae80baa0c40469a06ae2b8296
Bo Jakobsen <boj@ruc.dk>
2023-12-10 20:38:40 +0100

    Properly solution to part 2, but slow.

--- src/puz_8.py
+++ src/puz_8.py
@@ -1,4 +1,4 @@
-#with open('testdata/5_testdata.dat') as f:
+#with open('testdata/8_2_testdata.dat') as f:
 with open('data/8_data.dat') as f:
     lines=[x.strip() for x in f]
 
@@ -13,17 +13,51 @@ for kk in range(2,len(lines)):
     node0,node1=node.split(',')
     map[lab.strip()] = (node0.strip(),node1.strip())
 
+def part1():
+    steps=0
+    lab='AAA'
+    kk=0
+    while lab != 'ZZZ':
+        node=map[lab]
+        lab=node[inst[kk]]
+        kk+=1
+        if kk == len(inst):
+            kk=0
+        steps += 1
 
-steps=0
-lab='AAA'
-kk=0
-while lab != 'ZZZ':
-    node=map[lab]
-    lab=node[inst[kk]]
-    kk+=1
-    if kk == len(inst):
-        kk=0
-    steps += 1
+    print(steps)
+    return steps
 
-print(steps)
+# part1()
+
+def part2():
+    labs=[] # new start labs
+    Zlabs=[]
+    for lab in map.keys() :
+        if lab[2]=='A':
+            labs.append(lab)
+        elif lab[2]=='Z':
+            Zlabs.append(lab)
+
+    steps=0
+    kk=0
+    allfinished=False
+    while not allfinished:
+        allfinished = True
+        for k in range(len(labs)):
+            node=map[labs[k]]
+            labs[k]=node[inst[kk]]
+            if labs[k] not in Zlabs:
+                allfinished=False
+        kk+=1
+        if kk == len(inst):
+            kk=0
+        steps += 1
+    print(labs)
+    print(steps)
+
+part2()
+
+
+
 
