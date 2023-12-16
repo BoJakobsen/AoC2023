c32e80ddf88d4a07af8ff17f1a3168604888a32d
Bo Jakobsen <boj@ruc.dk>
2023-12-16 18:55:50 +0100

    halfway second part

--- src/puz_15.py
+++ src/puz_15.py
@@ -7,7 +7,7 @@ def HASH(string):
         val *=17
         val = val % 256
     return val
-    
+
 def part1():
     # as input is one long line, read one char at the time
     #with open('testdata/15_testdata.dat') as f:
@@ -25,7 +25,47 @@ def part1():
                     break
             HASHVal= HASH(string)
             sumit+=HASHVal
-            print(string,HASHVal)
+            #print(string,HASHVal)
     print(sumit)
 
-part1()
+#part1()
+
+
+boxs={x:[] for x in range(256) }
+# as input is one long line, read one char at the time
+#with open('../testdata/15_testdata.dat') as f:
+with open('../data/15_data.dat') as f:
+#    sumit=0    
+    while True:
+        string=''
+        ch=f.read(1)        
+        if not ch:
+            break
+        while (ch !=',')  and (ch !='\n'):
+            string+= ch
+            ch=f.read(1)
+            if not ch:
+                break
+        if string.find('=')>=0 : # case of assigning
+            N=string.find('=')
+            lab=string[0:N]
+            box=boxs[HASH(lab)]
+            fl=string[N+1:]
+            found=False
+            for Nlens in range(len(box)):
+                if box[Nlens][0] == lab:
+                    box[Nlens]=[lab, fl]
+                    found=True
+            if not found:
+                box.append([lab, fl])            
+        elif string.find('-')>=0 : # case of removing
+            N=string.find('-')
+            lab=string[0:N]
+            box=boxs[HASH(lab)]
+            for Nlens in range(len(box)):
+                if box[Nlens][0] == lab:
+                    box.pop(Nlens)
+                    break
+        else :
+            print('ERROR') 
+
