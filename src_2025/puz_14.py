89181bddac8b56d4ac15024bf08e04596afd10b7
Bo Jakobsen <boj@f77.dk>
2025-10-16 21:26:30 +0200

    Solve part 2 using dict for looking for a cycle

--- src_2025/puz_14.py
+++ src_2025/puz_14.py
@@ -1,13 +1,13 @@
 import re
 from timeit import default_timer as timer
 #
-with open('../testdata/14_testdata.dat') as f:
-#with open('../data/14_data.dat') as f:
+#with open('../testdata/14_testdata.dat') as f:
+with open('../data/14_data.dat') as f:
     lines=[list(x.strip()) for x in f]
 
 
-
-def move_right(lines_f):
+# Move round stones as fare as possible to the left 
+def move_left(lines_f):
     for k in range(len(lines_f)):
         line_f_str = "".join(list(lines_f[k]))
         O_pos = [match.start() for match in re.finditer(r'O', line_f_str)]
@@ -31,19 +31,23 @@ def move_right(lines_f):
         lines_f[k] = line
 
 
+# Clockwise rotation
 def cw_rot(lines_f):
     return [list(row) for row in zip(*lines_f[::-1])]
 
+# Counter clockwise rotation
 def ccw_rot(lines_f):  # counter clockwise
     return list(zip(*lines_f))[::-1]
 
 
+# Solve part 1
 def part1():
     lines_f = lines.copy()
-    # now uses rotations much cleaner
+    # One cc rotation to bring north to the left
     lines_f = ccw_rot(lines_f)
-    move_right(lines_f)
+    move_left(lines_f)
 
+    # this counting still assumes North to the left.
     res = 0
     for line in lines_f:
         for id, ch in enumerate(line):
@@ -52,30 +56,29 @@ def part1():
     print(res)
 
 
-#part1()
+part1()
 
-
+# Define the spin cycle
 def cycle(lines_f):
     # North
-    move_right(lines_f)
+    move_left(lines_f)
 
     # West
     lines_f = cw_rot(lines_f)
-    move_right(lines_f)
+    move_left(lines_f)
 
     # South
     lines_f = cw_rot(lines_f)
-    move_right(lines_f)
+    move_left(lines_f)
 
     # East
     lines_f = cw_rot(lines_f)
-    move_right(lines_f)
+    move_left(lines_f)
 
     # back to north
     lines_f = cw_rot(lines_f)
 
     return lines_f
-
 
 
 def printit(lines_f):
@@ -84,20 +87,22 @@ def printit(lines_f):
     print()
 
     
-# run 3 cycle on the test data for comparing to problem formulatoin 
+# run 3 cycle on the test data for comparing to problem formulation 
 def part2_test():
-    #  copy and make one counter clocwise rotation to bring northe to the left
     lines_f = lines.copy()
 
     printit(lines_f)
     for kk in range(0,3):
+        # rotate North back and forward for printing upright 
         lines_f = ccw_rot(lines_f)
         lines_f = cycle(lines_f)
         lines_f = cw_rot(lines_f)
         printit(lines_f)
 
 
-# Make N cycles no printout
+# part2_test()
+
+# Make N cycles no printout for timing
 def part2_test2(N):
     #  copy and make one counter clocwise rotation to bring northe to the left
     lines_f = lines.copy()
@@ -109,21 +114,49 @@ def part2_test2(N):
 
 
 # Time it
-start = timer()
-part2_test2(1000)
-end = timer()
-print(end - start) # Time in seconds, e.g.
-# 0.35 s for 1000 so 1000.000.000 is our of the question this way
+# start = timer()
+# part2_test2(1000)
+# end = timer()
+# print(end - start) # Time in seconds, e.g.
+# 0.35 s for 1000 on test data so 1000.000.000 is our of the question this way
+# 7.7 s for 1000 on real data so 1000.000.000 is our of the question this way
 
 
-
-# # res = 0
-# # for line in lines_f:
-# #     for id, ch in enumerate(line):
-# #         if ch == 'X':
-# #             res += len(line) - id
-# # print(res)
+def part2():
+    # Looking for a cycle requires a non-mutable key.
+    def matrix_to_key(matrix):
+        """Convert matrix to hashable tuple"""
+        return tuple(tuple(row) for row in matrix)
 
 
+    #  Make one counter clocwise rotation to bring northe to the left
+    lines_f = lines.copy()
+    lines_f = ccw_rot(lines_f)
 
+    # Look for a cycle (if this does not exits this will not end in reasonable time)
+    seenstates = {}
+    N = 1000_000_000
+    for kk in range(0,N):
+        key = matrix_to_key(lines_f)
+        if key in seenstates:
+            print('Cycle found', seenstates[key], kk)
+            Nstart = seenstates[key]
+            Ncyc = kk - seenstates[key]
+            CycState = key
+            break
+        seenstates[key] = kk
+        lines_f = cycle(lines_f)
 
+    # Perform missing cycles for N in total skipping integer numbers of cycles 
+    for kk in range(0, (N-kk) % Ncyc):
+        lines_f = cycle(lines_f)
+
+    # Count the "load on the north beam"
+    res = 0
+    for line in lines_f:
+        for id, ch in enumerate(line):
+            if ch == 'O':
+                res += len(line) - id
+    print(res)
+
+part2()
