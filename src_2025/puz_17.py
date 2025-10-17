f03c6a89e4199f6ef88aa1c455ef9b4b2b63848f
Bo Jakobsen <boj@f77.dk>
2025-10-17 22:11:44 +0200

    Include part two, same idea updated definition of possible moves

--- src_2025/puz_17.py
+++ src_2025/puz_17.py
@@ -77,3 +77,76 @@ def prob1():
 
 
 prob1()
+
+
+# Part 2
+# Same type of solution, but more complex rules
+def prob2():
+    # Priority queue: (distance, row, col, direction, Ndir )
+    pq = [(0, *pos0, (0, 0), 0)]
+
+    # Track shortest distance to each position
+    distances = {pos0: 0}
+
+    # Visited set
+    visited = set()
+
+    # Directions: up, right, down, left
+    directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
+    while pq:
+        dist, r, c, d, Ndir = heapq.heappop(pq)
+
+        # Skip if already visited
+        if (r, c, d, Ndir) in visited:
+            continue
+
+        visited.add((r, c, d, Ndir))
+
+        # Found destination
+        if (r, c) == end0:
+            print(dist)
+            break
+            #return dist
+
+        # Check all possible moves
+
+        for dnew in directions:
+            # check the conditions from the puzzle
+            if dnew == (-1*d[0], -1*d[1]):  # we can't go back same direction
+                continue
+            if Ndir == 10 and dnew == d:  # if we already moved 10 times in that dir skip
+                continue
+            if Ndir < 4 and dnew != d and d != (0, 0):  # we still need to go same dir            
+                continue
+
+            dr, dc = dnew
+            nr, nc = r + dr, c + dc
+
+            # Check boundary 
+            if 0 > nr or nr >= rows or 0 > nc or nc >= cols:
+                continue
+
+            # If new direction check that we have enough space to move 4 
+            if d != dnew and (0 > r + dr*4 or r + dr*4 >= rows or 0 > c + dc*4 or c + dc*4 >= cols):
+                continue
+
+            if dnew == d:  #
+                newNdir = Ndir + 1
+            else:
+                newNdir = 1
+
+            if (nr, nc, dnew, newNdir) in visited:
+                continue
+
+            # Cost is current distance + cost to enter new cell
+            new_dist = dist + int(ma[nr][nc])
+
+            # Update if shorter path found
+            # remember that this needs to be the full state leading to a node
+            if (nr, nc, dnew, newNdir) not in distances or new_dist < distances[(nr, nc, dnew, newNdir)]:
+                distances[(nr, nc, dnew, newNdir)] = new_dist
+                heapq.heappush(pq, (new_dist, nr, nc, dnew, newNdir))
+
+
+prob2()
